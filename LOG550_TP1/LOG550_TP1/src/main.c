b5b887634e78d472877f1c208b9c33b3441c5d19
Christopher Lariviere <lariviere.c@gmail.com>
2015-02-25 21:53:34 -0500

    Commited from home, new changes lab should be done

--- LOG550_TP1/LOG550_TP1/src/main.c
+++ LOG550_TP1/LOG550_TP1/src/main.c
@@ -1,14 +1,157 @@
+TODAY
+
+Youuploaded an item
+21:01
+C
+main.c
+EARLIER THIS WEEK
+
+Samy Lemcelliedited an item
+Mon 21:27
+Google Docs
+Cours 6
+
+Samy Lemcellicreated an item in
+Mon 18:04
+Google Drive Folder
+LOG550
+Created items:
+Google Docs
+Cours 6
+
+Youedited an item
+Sun 16:14
+Google Docs
+Revision Intra
+
+Youedited an item
+Sun 15:50
+Google Docs
+Cours 5
+
+Youmoved an item to
+Sun 15:26
+Google Drive Folder
+LOG550
+Moved items:
+Google Docs
+Revision Intra
+
+Youedited an item
+Sun 15:24
+Google Docs
+Revision Intra
+
+Youcreated an item in
+Sun 14:05
+Google Drive Folder
+Chapitre 4
+Created items:
+Google Docs
+Revision Intra
+LAST WEEK
+
+Youedited an item
+17 Feb
+Google Docs
+Plan scolaire
+
+Samy Lemcelliedited an item
+16 Feb
+Google Docs
+Cours 5
+
+Samy Lemcellicreated an item in
+16 Feb
+Google Drive Folder
+LOG550
+Created items:
+Google Docs
+Cours 5
+EARLIER THIS MONTH
+S
+Simon Richard-Girouxmoved 2 items to the bin
+12 Feb
+Google Docs
+GTI350_ModeleRapportLab4
+Google Docs
+GTI350_ModeleRapportLab3
+
+Youadded an item to
+11 Feb
+Google Drive Folder
+LOG550
+Added items:
+Google Drive Folder
+LOG550
+
+Youcreated an item in
+Computer • 6 Feb
+Google Drive Folder
+Winter 2015
+Created items:
+PDF
+Proto1_DescriptionSeance_1_H2015.pdf
+LAST MONTH
+
+Youcreated an item in
+Computer • 31 Jan
+Google Drive Folder
+Personal
+Created items:
+PDF
+DepotDirecteChristopherLariviere.pdf
+
+Youmoved an item to the bin
+29 Jan
+Google Sheets
+Untitled spreadsheet
+
+Youcreated an item in
+29 Jan
+Google Drive Folder
+School
+Created items:
+Google Sheets
+Untitled spreadsheet
+
+Youmoved an item to the bin
+Mobile • 25 Jan
+Unknown File
+American.Sniper.2014.DVDSCR.XviD.AC3-EVO.torrent
+
+Youcreated 8 items in
+Computer • 25 Jan
+Google Drive Folder
+My Scans
+Created items:
+PDF
+Triotech.pdf
+PDF
+passport.pdf
+PDF
+proof of residency.pdf
+Image
+opus.bmp
+PDF
+DepotDirecteChristopherLariviere.pdf
+PDF
+CMC_Electronique.pdf
+Show all...
+
+Youcreated an item in
+Computer • 25 Jan
+Google Drive Folder
+PC Stuff
+Created items:
+Google Drive Folder
+My Scans
+Show more activity
+2 GB used (6%)
+Buy more storage
+main.cOpen
 /**
-*/
-
-/*
-	Comments: 
-	interrupt adc
-	interruip pb0
-	interrupt reception et fin de seriel -> reception fait
-
-	timer 1 ou 2 -> seulement un timer
-
+* Bonjour :)
 */
 
 #include <asf.h>
@@ -26,7 +169,10 @@
 #define FPBA                        (FOSC0 >> 4)
 #define FALSE						0
 
-#define FOURHERTZ_TC5				FPBA / 40 //Every second, 4 clicks
+#define LIGHT_CHANNEL				2
+#define POT_CHANNEL					1
+
+#define FOURHERTZ_TC5				(FOSC0 >> 7) / 8 //Every second, 4 clicks toggle twice
 #define TWOKHERTZ_TC4				FPBA / 4000
 #define ONEKHERTZ_TC4				FPBA / 2000
 #define FOURKHERTZ_TC4				FPBA / 9000
@@ -52,6 +198,7 @@
 #define START_ADC_CONVERSION		1 << 6
 #define DATA_ACQUISITION_RDY		1 << 7
 #define ALREADY_ON_FLAG				1 << 8
+#define USART_RDY					1 << 9
 
 #define CLEAR_PUSH_BUTTON			(booleanValues &= ~0x01)
 #define CLEAR_DEPASSEMENT_UART_FLAG	(booleanValues &= ~0x02)
@@ -62,6 +209,7 @@
 #define CLEAR_ADC_CONVERSION_FLAG	(booleanValues &= ~0x40)
 #define CLEAR_DATA_ACQUISITION_FLAG	(booleanValues &= ~0x80)
 #define CLEAR_ALREADY_ON			(booleanValues &= ~0x100)
+#define CLEAR_USART_RDY				(booleanValues &= ~0x200)
 
 #define IS_PUSHED					(booleanValues == (booleanValues | PUSHED_PB_0))
 #define IS_DEPASSEMENT_UART			(booleanValues == (booleanValues | DEPASSEMENT_UART))
@@ -72,12 +220,7 @@
 #define IS_ADC_STARTED				(booleanValues == (booleanValues | START_ADC_CONVERSION))
 #define IS_DATA_ACQUISITION_RDY		(booleanValues == (booleanValues | DATA_ACQUISITION_RDY))
 #define IS_ALREADY_ON				(booleanValues == (booleanValues | ALREADY_ON_FLAG))
-
-/************************************************************************/
-/* ADC Light Channel configurations     UC3A0512.h                      */
-/************************************************************************/
-
-#define LIGHT_CHANNEL				2
+#define IS_USART_RDY				(booleanValues == (booleanValues | USART_RDY))
 
 // Fonctions preprocesseur
 #define TOGGLE_LED(ledNumber)			(LED_Toggle(ledNumber))
@@ -151,17 +294,12 @@ static const gpio_map_t USART_GPIO_MAP =
 	{AVR32_USART1_TXD_0_0_PIN, AVR32_USART1_TXD_0_0_FUNCTION}
 };
 
-//Flags
-
-U8 lightSensorSent = 0;
-
 //Variables
 
 U32 incomingSerialValue;
-U8 compteur;
 
 volatile U32 lightSensorValue = 0;
-volatile U32 potValue = 0;	
+volatile U32 potValue = 0;
 
 volatile avr32_tc_t *tc0 = TIMER_COUNTER;
 
@@ -178,37 +316,18 @@ static void irq_led(void)
 
 __attribute__((__interrupt__))
 static void irq_serial_communication(void)
-{	
-	
-	if(AVR32_USART1.csr & (AVR32_USART_CSR_TXRDY_MASK))
-	{
-		booleanValues |= DEPASSEMENT_UART;
-	}
+{
 	//Receiving
 	if (AVR32_USART1.csr & (AVR32_USART_CSR_RXRDY_MASK))
 	{
 		incomingSerialValue = (AVR32_USART1.rhr & AVR32_USART_RHR_RXCHR_MASK);
 	}
+	//transmitting
 	else
 	{
-		//Transmitting
-		if(IS_POTENTIOMETER_VAL_RDY)
-		{
-			AVR32_USART1.thr = potValue & AVR32_USART_THR_TXCHR_MASK;
-			CLEAR_POTENTIOMETER_FLAG;
-		}
-		else if(IS_SENSOR_VAL_RDY)
-		{
-			
-			AVR32_USART1.thr = 	lightSensorValue & AVR32_USART_THR_TXCHR_MASK;
-			CLEAR_SENSOR_FLAG;
-			lightSensorSent = 1;
-		}
-		else
-		{
-			AVR32_USART1.idr = AVR32_USART_IDR_TXRDY_MASK;
-		}
+		AVR32_USART1.idr = AVR32_USART_IDR_TXRDY_MASK;
 	}
+	booleanValues |= USART_RDY;
 }
 
 __attribute__((__interrupt__))
@@ -231,17 +350,26 @@ static void irq_adc_channel(void)
 	}
 	
 	U32 statusRegister = AVR32_ADC.sr;
+	
+	//potentiometer conversion done
+	if(statusRegister & AVR32_ADC_IER_EOC1_MASK)
+	{
+		if(IS_POTENTIOMETER_VAL_RDY  && !(AVR32_USART1.csr & (AVR32_USART_CSR_TXRDY_MASK)))
+		{
+			booleanValues |= DEPASSEMENT_UART;
+		}
+		potValue = AVR32_ADC.cdr1;
+		booleanValues |= POTENTIOMETER_VAL_RDY;
+	}
 	//light sensor conversion done
 	if(statusRegister & AVR32_ADC_IER_EOC2_MASK)
 	{
+		if(IS_SENSOR_VAL_RDY && !(AVR32_USART1.csr & (AVR32_USART_CSR_TXRDY_MASK)))
+		{
+			booleanValues |= DEPASSEMENT_UART;
+		}
 		lightSensorValue = AVR32_ADC.cdr2;
 		booleanValues |= SENSOR_VAL_RDY;
-	}
-	//potentiometer conversion done
-	else if(statusRegister & AVR32_ADC_IER_EOC1_MASK)
-	{
-		potValue = AVR32_ADC.cdr1;
-		booleanValues |= POTENTIOMETER_VAL_RDY;
 	}
 	CLEAR_ADC_CONVERSION_FLAG;
 }
@@ -260,9 +388,14 @@ static void irq_adc_timer(void)
 void adc_init(void)
 {
 	pcl_switch_to_osc(PCL_OSC0, FOSC0, OSC0_STARTUP);
-	adc_configure(&AVR32_ADC);
-	adc_enable(&AVR32_ADC, ADC_LIGHT_CHANNEL);
-	adc_enable(&AVR32_ADC, ADC_POTENTIOMETER_CHANNEL);
+	//Configure the ADC to use 8 bit values instead of 10
+	AVR32_ADC.mr |= 1 << AVR32_ADC_LOWRES_OFFSET;
+	
+	//Enable the Light Sensor
+	AVR32_ADC.cher = 1 << LIGHT_CHANNEL;
+	//Enable the potentiometer
+	AVR32_ADC.cher = 1 << POT_CHANNEL;
+	
 	
 	INTC_register_interrupt(&irq_adc_channel,AVR32_ADC_IRQ,P_HIGH);
 	AVR32_ADC.ier = AVR32_ADC_IER_EOC2_MASK | AVR32_ADC_IER_EOC1_MASK;
@@ -277,7 +410,7 @@ void timercounter_init(void)
 	current = 2;
 	
 	tc_init_waveform(tc0, &WAVEFORM_OPT_1);
-	//4Hz = 4 cycles / second
+	
 	tc_write_rc(tc0, TC_CHANNEL_1, FOURHERTZ_TC5);
 	tc_configure_interrupts(tc0, TC_CHANNEL_1, &TC_INTERRUPT_0);
 	tc_start(tc0, TC_CHANNEL_1);
@@ -292,7 +425,7 @@ void usart_init(void)
 {
 	pm_switch_to_osc0(&AVR32_PM, FOSC0, OSC0_STARTUP);
 	gpio_enable_module(USART_GPIO_MAP,sizeof(USART_GPIO_MAP) / sizeof(USART_GPIO_MAP[0]));
-	init_dbg_rs232(FOSC0);
+	init_dbg_rs232_ex(57600,FOSC0);
 	
 	INTC_register_interrupt(&irq_serial_communication, AVR32_USART1_IRQ, P_LOWEST);
 	
@@ -310,18 +443,18 @@ void intialization(void)
 {
 	Disable_global_interrupt();
 	
-	INTC_init_interrupts();	
+	INTC_init_interrupts();
 	
 	adc_init();
 	push_button_init();
 	usart_init();
 	timercounter_init();
 	
-	Enable_global_interrupt();	
+	Enable_global_interrupt();
 	
 	//LED_Toggle(LED1);
 	LED_Off(LED4);
-	print_dbg("Taper S ou X pour demarrer l'acquisition de donnees: \n");
+	print_dbg("\nTaper S ou X pour demarrer l'acquisition de donnees: \n");
 }
 
 int main (void)
@@ -340,7 +473,7 @@ int main (void)
 			TOGGLE_LED(LED0);
 			if(IS_DATA_ACQUISITION_RDY)
 			{
-				TOGGLE_LED(LED1);	
+				TOGGLE_LED(LED1);
 			}
 			CLEAR_LED_FLAG;
 		}
@@ -359,15 +492,26 @@ int main (void)
 			booleanValues |= ALREADY_ON_FLAG;
 			booleanValues |= DATA_ACQUISITION_RDY;
 		}
-		
-		//if(sensorValueReady || (potValueReady && lightSensorSent))
-		if(IS_SENSOR_VAL_RDY || (POTENTIOMETER_VAL_RDY && lightSensorSent))
+		//Transmitting
+		if(IS_USART_RDY)
 		{
-			/*if(AVR32_USART1.csr & (AVR32_USART_CSR_TXRDY_MASK))
+			if(IS_SENSOR_VAL_RDY && IS_USART_RDY)
 			{
-				booleanValues |= DEPASSEMENT_UART;
-			}*/
-			AVR32_USART1.ier = AVR32_USART_IER_TXRDY_MASK;
+				
+				AVR32_USART1.thr = 	lightSensorValue | 0x01;
+				CLEAR_SENSOR_FLAG;
+				CLEAR_USART_RDY;
+				
+				AVR32_USART1.ier = AVR32_USART_IER_TXRDY_MASK;
+			}
+			if(IS_POTENTIOMETER_VAL_RDY  && IS_USART_RDY)
+			{
+				AVR32_USART1.thr = potValue & ~0x01;
+				CLEAR_POTENTIOMETER_FLAG;
+				CLEAR_USART_RDY;
+				
+				AVR32_USART1.ier = AVR32_USART_IER_TXRDY_MASK;
+			}
 		}
 		
 		//If we pressed the PB0, we change the hertz
@@ -377,16 +521,16 @@ int main (void)
 			switch(current)
 			{
 				case 1:
-					tc_write_rc(tc0, TC_CHANNEL_2, TWOKHERTZ_TC4);
-					current = 2;
-					break;
+				tc_write_rc(tc0, TC_CHANNEL_2, TWOKHERTZ_TC4);
+				current = 2;
+				break;
 				case 2:
-					tc_write_rc(tc0, TC_CHANNEL_2, ONEKHERTZ_TC4);
-					current = 1;
-					break;
+				tc_write_rc(tc0, TC_CHANNEL_2, ONEKHERTZ_TC4);
+				current = 1;
+				break;
 			}
 		}
-				
+		
 		// Dépassements
 		if(IS_DEPASSEMENT_ADC)
 		{
