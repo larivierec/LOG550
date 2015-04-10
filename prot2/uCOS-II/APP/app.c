414affa3a01cfd96c3c3e7ec59a1b36497873ac9
Christopher Lariviere <lariviere.c@gmail.com>
2015-04-09 20:03:03 -0400

    added all the comments and last hex before sending

--- prot2/uCOS-II/APP/app.c
+++ prot2/uCOS-II/APP/app.c
@@ -1,103 +1,153 @@
-/************************************************************************/
-/*                                                                      */
-/************************************************************************/
+/**
+	Fichier: app.c
+	Auteur:	Christopher Lariviere, Samy Lemcelli
+	Date:	4/9/2015
+*/
 
-#include  "includes.h"   // Ceci inclu os_cfg.h, config des services noyaux UCOS-II...
+/** @mainpage
+<center><b>Laboratoire de systemes informatiques en temps reel</b></center>
 
-/**** Reservation du STACK de chaque tache ************************************************/
+@author <ul> <li> Christopher Lariviere <li>Samy Lemcelli </ul>
+
+@section MainSection1 Description
+
+Le but du systeme sur le microcontroleur est de convertir le signal analogique
+en donnees numeriques avec l'ADC et de les transferer vers le PC par lien en serie soit en:
+
+Le but du deuxieme laboratoire de LOG550 est identique a la premiere.\n
+Nous n'utilisons pas directement l'ASF (Atmel Software Framework), nous utilisons plutot
+un noyau uCOS-II qui interface avec le EVK1100
+
+-
+Vitesse de transfert utilisée
+<ul>
+<li> 115200 bauds (Bonus - USART0)</li>
+<li> 57600 bauds (ADC - USART1)</li>
+</ul>
+- <b>Logiciel necessaire</b> : AVR-Studio 6.2, AVR-GCC, Oscilloscope JAVA, TeraTerminal PRO pour Bonus
+- <b>Materiel necessaire</b> : ATmega32 on EVK1100 board
+- <b>Librairies/Modules necessaire</b> :
+<ul>
+<li> uCOS-II de Microcium qui est inclut dans le fichier zip.
+</ul>
+
+-
+
+Specifications
+<ul>
+<li> 2 canaux operant 500 echantillons/sec
+<li> Tout acces a une ressource partagee requiert un semaphore
+<li> L'utilisation d'un FIFO fournis par le noyau uCOS-II
+<li> 5 taches dont: LED_Flash, UART_Cmd_RX, UART_SendSample, ADC_Cmd(), Alarm_MsgQ
+<li> 1 tache (boni): Bonus_Thread() qui s'occupe d'afficher en terminal de l'utilisation CPU et Le nombre de context switch
+</ul>
+
+@file app.c Contient le programme de LOG550 developpe par Christopher Lariviere et Samy Lemcelli
+*/
+#include  "includes.h"   /**<Ceci inclu os_cfg.h, config des services noyaux UCOS-II...*/
 
 #define LIGHT_CHANNEL				2			/**< Le numero du channel de la lumiere */
 #define POT_CHANNEL					1			/**< Le numero du channel du potentiometre */
 
-volatile CPU_INT32U booleanFlags = 0;
+/** Variables Globales */
 
+/**
+	@brief defini le stack de chacun des taches
+*/
 #define OS_TASK_STK_SIZE 512
 
-OS_STK  LED_Stack[OS_TASK_STK_SIZE];
-OS_STK  UART_RX_Stack[OS_TASK_STK_SIZE];
-OS_STK  UART_TX_Stack[OS_TASK_STK_SIZE];
-OS_STK  ADC_Stack[OS_TASK_STK_SIZE];
-OS_STK  Alarm_Stack[OS_TASK_STK_SIZE];
-OS_STK  Bonus_Strack[OS_TASK_STK_SIZE];
+OS_STK  LED_Stack[OS_TASK_STK_SIZE];					/**< Declaration du stack LED_Stack */
+OS_STK  UART_RX_Stack[OS_TASK_STK_SIZE];			/**< Declaration du stack UART_RX_Stack*/
+OS_STK  UART_TX_Stack[OS_TASK_STK_SIZE];			/**< Declaration du stack UART_TX_Stack*/
+OS_STK  ADC_Stack[OS_TASK_STK_SIZE];					/**< Declaration du stack ADC_Stack*/
+OS_STK  Alarm_Stack[OS_TASK_STK_SIZE];				/**< Declaration du stack Alarm_Stack*/
+OS_STK  Bonus_Stack[OS_TASK_STK_SIZE];				/**< Declaration du stack Bonus_Stack*/
 
+/**
+	@brief	La tache LED_flash qui toggle a chaque 100 ms -> 200 ms pour un toggle complet
+*/
+static void LED_flash(void *p_arg);
 
-static  void  LED_flash(void *p_arg);
-static  void  UART_Cmd_RX(void *p_arg);
-static  void  UART_SendSample(void *p_arg);
-static  void  ADC_Cmd(void *p_arg);
-static	void  Alarm_msgQ(void *p_arg);
-static	void  Bonus_Thread(void *p_arg);
+/**
+	@brief gere la reception des donnes par l'interface de communication USART
+*/
+static void UART_Cmd_RX(void *p_arg);
 
-/** Semaphores */
+/**
+	@brief Envoi le un sample du MessageQueue (ADCMsg) sur le USART1 a 57600 bauds
+*/
+static void UART_SendSample(void *p_arg);
 
-OS_EVENT	*Start_Sem;		// Conversion has started
-OS_EVENT	*UART_TX_Sem;		// Semaphore
-OS_EVENT	*Alarm_Sem;		// Alarm has been triggered or not
-OS_EVENT	*LED1_Sem;
-OS_EVENT	*SEM_ADC_Alternate;
+/**
+	@brief Converti un canal du adc lumiere OU potentiometre a chaque milliseconde
+*/
+static void ADC_Cmd(void *p_arg);
 
-OS_EVENT	*Mbox1;		// Semaphore
-OS_EVENT	*ADCMsg;		// Mailbox
+/**
+	@brief Nous averti s'il y a eu un depassement au niveau du ADC (ne devrait jamais se reveiller)
+*/
+static void Alarm_msgQ(void *p_arg);
 
-void  *ADCMsgFIFO[4];		// Ma MessageQueue (FIFO)
-void Init_IO_Usager(void);          // Fonction definie localement
+/**
+	@brief Nous affiche sur le USART0 a 115200 bauds l'usage CPU et le nombre de context switch \n
+	a chaque 500 ms
+*/
+static void Bonus_Thread(void *p_arg);
 
-/**** Fonction principale main()***********************************************************/
-int  main (void)
+/**<Semaphores */
+
+OS_EVENT	*Start_Sem;		/**<Semaphore de debut de conversion */
+OS_EVENT	*UART_TX_Sem;		/**<Semaphore de transmission UART1*/
+OS_EVENT	*Alarm_Sem;		/**<Semaphore d'alarm (qui ne devrait pas actionner)*/
+OS_EVENT	*LED1_Sem;		/**<Semphore des LEDs*/
+OS_EVENT	*SEM_ADC_Alternate	/**<Semaphore d'alternance pour le ADC (canaux)*/;
+
+OS_EVENT	*ADCMsg;		/**<Semaphore pour MessageQueue pour ADC*/
+
+void *ADCMsgFIFO[4];		/**<MessageQueue pour l'ADC (FIFO) */
+void Init_IO_Usager(void);
+
+/**
+	@brief	Point d'entree pour le programme
+*/
+
+int main (void)
 {
-	// Initialize the needed elements
-	BSP_Init();         // Set le timer et demarre les ISR_TICKS.
-	// Doit absolument etre appele par la premiere tache a operer.
-	// Attention, desormais on roule a 48MHz et USART a 48/2MHz
-	CPU_IntDis();       /* Desactive toute les interrupts pendant l'initialisation  */
-	OSInit();           /* Initialise "uC/OS-II, The Real-Time Kernel"              */
-	Init_IO_Usager();   // Initialisation des differents I/O par l'usager
-	
-	
-	/* Creation de toute les taches...a des priorites differentes (1<prio<25) */
+	BSP_Init();         /*Set le timer et demarre les ISR_TICKS.*/
+	CPU_IntDis();       /*Desactive toute les interrupts pendant l'initialisation  */
+	OSInit();           /*Initialise "uC/OS-II, The Real-Time Kernel"              */
+	Init_IO_Usager();   /*Initialisation des differents I/O par l'usager					*/
+
+	/*Creation de toute les taches, a des priorites differentes (1<prio<25) */
 	OSTaskCreate(LED_flash, NULL, (OS_STK *)&LED_Stack[OS_TASK_STK_SIZE-1], 1);
 	OSTaskCreate(UART_Cmd_RX, NULL, (OS_STK *)&UART_RX_Stack[OS_TASK_STK_SIZE-1], 2);
 	OSTaskCreate(UART_SendSample, NULL, (OS_STK *)&UART_TX_Stack[OS_TASK_STK_SIZE-1], 3);
 	OSTaskCreate(ADC_Cmd, NULL, (OS_STK *)&ADC_Stack[OS_TASK_STK_SIZE-1], 4);
 	OSTaskCreate(Alarm_msgQ, NULL,(OS_STK *)&Alarm_Stack[OS_TASK_STK_SIZE-1], 5);
-	OSTaskCreate(Bonus_Thread, NULL,(OS_STK *)&Bonus_Strack[OS_TASK_STK_SIZE-1], 6);
+	OSTaskCreate(Bonus_Thread, NULL,(OS_STK *)&Bonus_Stack[OS_TASK_STK_SIZE-1], 6);
 
-	Start_Sem   = OSSemCreate(0);	// Conversion has started
-	UART_TX_Sem = OSSemCreate(0);	// Transfer Ready
-	Alarm_Sem = OSSemCreate(0);		// Alarm has been triggered or not
-	LED1_Sem = OSSemCreate(0);		// LED1 state for LED2
-	SEM_ADC_Alternate = OSSemCreate(0); // State of the ADC
-	
-	Mbox1   = OSMboxCreate(NULL);   // MailBox,   initialisation
-	ADCMsg   = OSQCreate(ADCMsgFIFO, 4); // Message Queue of the ADC Initialized
-	
-	
 
-	OSStart();          /* Demarre le multitasking (Donne le controle au noyau uC/OS-II)  */
-	// Le code ici ne sera jamais execute
-	return (0);         /* Pour eviter le warnings en GCC, prototype (int main (void))    */
+	Start_Sem   = OSSemCreate(0);				/*Creation du semaphore Start_Sem*/
+	UART_TX_Sem = OSSemCreate(0);				/*Creation du semaphore UART_TX_Sem*/
+	Alarm_Sem = OSSemCreate(0);					/*Creation du semaphore Alarm_Sem*/
+	LED1_Sem = OSSemCreate(0);					/*Creation du semaphore LED1_Sem*/
+	SEM_ADC_Alternate = OSSemCreate(0);	/*Creation du semaphore SEM_ADC_Alternate*/
+	ADCMsg = OSQCreate(ADCMsgFIFO, 4);	/*Creation du message queue de taille 4 ADCMsg */
+
+	OSStart();
+	return (0);
 }
 
-/******************************************************************************************/
-/* Definition des taches sous UCOS-II                                                     */
-/*  - Toutes les taches ont une priorite differente                                       */
-/*  - Le scheduler de UCOS-II execute TOUJOURS la tache la plus prioritaire qui est READY.*/
-/******************************************************************************************/
 
-/******************************************************************************************/
-static  void  LED_flash (void *p_arg)
+static void LED_flash (void *p_arg)
 {
-	(void)p_arg;          // Pour eviter le warnings en GCC
+	(void)p_arg;
 	CPU_INT08U   err, MsgQDataRX;
 
-
 	OSStatInit();
-	
 
-	/*---Fin de l'initialisation----------*/
+	while (1) {
 
-	while (1) {           // Tache, une boucle infinie.
-		
 		LED_Toggle(1);
 		if(LED1_Sem->OSEventCnt == 1) // Have to track LED1 state. It was done by LED.h in old lab.
 		{
@@ -107,25 +157,24 @@ static  void  LED_flash (void *p_arg)
 		{
 			OSSemSet(LED1_Sem,1,NULL);
 		}
-		
+
 		if(Start_Sem->OSEventCnt == 1)
 		{
 			LED_Toggle(2);
 		}
-		
-
 		OSTimeDly(100);// 200ms == 5 fois secondes
 	}
 }
-/******************************************************************************************/
-static  void  UART_Cmd_RX (void *p_arg)
+
+
+static void UART_Cmd_RX (void *p_arg)
 {
 	(void)p_arg;
 	char MboxDataRX;
 
 	while (1) {
 		MboxDataRX=BSP_USART_ByteRd(1); // Read from USART1
-		
+
 		if(MboxDataRX == 's'){
 			OSSemSet(Start_Sem,1,NULL);
 			(LED1_Sem->OSEventCnt == 1) ? LED_On(2) : LED_Off(2);
@@ -134,11 +183,11 @@ static  void  UART_Cmd_RX (void *p_arg)
 			OSSemSet(Start_Sem,0,NULL);
 			LED_Off(2);
 		}
-		
+
 		OSTimeDly(200); // Shouldn't spam check so delay
 	}
 }
-/******************************************************************************************/
+
 static void Bonus_Thread(void *p_arg)
 {
 	(void)p_arg;
@@ -159,11 +208,9 @@ static void Bonus_Thread(void *p_arg)
 		OSCtxSwCtr = 0;
 		BSP_USART_printf(0, buffer);
 		BSP_USART_printf(0, "\n");
-
 	}
-	
 }
-/******************************************************************************************/
+
 static  void  UART_SendSample (void *p_arg)
 {
 	(void)p_arg;
@@ -174,59 +221,55 @@ static  void  UART_SendSample (void *p_arg)
 		BSP_USART_ByteWr(1, MsgQDataRX);
 	}
 }
-/******************************************************************************************/
-static  void  ADC_Cmd (void *p_arg)
+
+static void ADC_Cmd (void *p_arg)
 {
 	(void)p_arg;
-	
+
 	while (1) {
 		if(ADCMsg->OSEventCnt >= 4)
 		{
 			OSSemPost(Alarm_Sem);
 		}
-		
+
 		if(Start_Sem->OSEventCnt == 1)
 		{
-			AVR32_ADC.cr = AVR32_ADC_START_MASK;
+			AVR32_ADC.cr = AVR32_ADC_START_MASK; // starts the ADC Conversion
 			 // This is just to get a visual queue. Must delete.
-			CPU_INT32U statusRegister = AVR32_ADC.sr;			
+			CPU_INT32U statusRegister = AVR32_ADC.sr;
 			if((statusRegister & AVR32_ADC_IER_EOC1_MASK) && SEM_ADC_Alternate->OSEventCnt == 0)
 			{
 				OSSemSet(SEM_ADC_Alternate,1,NULL);
 				OSQPost(ADCMsg, (AVR32_ADC.cdr1 | 0x01));
 			}
-			
+
 			if((statusRegister & AVR32_ADC_IER_EOC2_MASK) && SEM_ADC_Alternate->OSEventCnt == 1)
 			{
 				OSSemSet(SEM_ADC_Alternate,0,NULL);
 				OSQPost(ADCMsg, (AVR32_ADC.cdr2 & ~0x01));
 			}
 		}
-		
 		OSTimeDly(1);        // Delai en TICKS (1 TICKS=1milisec)
 	}
 }
-/******************************************************************************************/
 
-static	void Alarm_msgQ(void *p_arg){
+static void Alarm_msgQ(void *p_arg){
 	(void)p_arg;
 	OSSemPend(Alarm_Sem,0,NULL);
 	LED_Toggle(3);
 }
 
+/**
+	@brief Initialise les GPIO pour l'usager
+*/
+
 void Init_IO_Usager(void)
 {
-	BSP_USART_Init(1,57600) ; //Initialise USART1 at 57600bauds, voir BSP.c
-	BSP_USART_Init(0,115200) ; //Initialise USART0 at 115200 bauds pour les points bonis
+	BSP_USART_Init(1,57600) ; //Initialise USART1 at 57600bauds
+	BSP_USART_Init(0,115200) ; //Initialise USART0 at 115200 bauds pour le bonus
 	BSP_USART_printf(1, "\nPrototype 2 : LOG550\n Samy Lemcelli, Christopher Lariviere");
 
-	AVR32_ADC.mr |= 1 << AVR32_ADC_LOWRES_OFFSET;
-	
-	//Enable the Light Sensor
-	AVR32_ADC.cher = 1 << LIGHT_CHANNEL;
-	//Enable the potentiometer
-	AVR32_ADC.cher = 1 << POT_CHANNEL;
-	
+	AVR32_ADC.mr |= 1 << AVR32_ADC_LOWRES_OFFSET;	//initialize the ADC to convert using only 8-bits
+	AVR32_ADC.cher = 1 << LIGHT_CHANNEL; //Enable the Light Sensor
+	AVR32_ADC.cher = 1 << POT_CHANNEL;	//Enable the potentiometer
 }
-
-
---
+++ prot2/uCOS-II/Release/APP/spi/spi.d
@@ -0,0 +1,160 @@
+APP/spi/spi.d APP/spi/spi.o APP/spi/spi.o: ../APP/spi/spi.c \
+ ../APP/spi/spi.h ../APP/includes.h ../APP/os_cfg.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/stdio.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/newlib.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/config.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/ieeefp.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stddef.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stdarg.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/reent.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/_types.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_types.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_default_types.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/lock.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/types.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/types.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/features.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/stdio.h \
+ ../uC-CPU/AVR32/UC3/AtmelStudio6/cpu.h ../uC-CPU/cpu_def.h \
+ ../uC-CPU/Cfg/Template/cpu_cfg.h ../APP/app_cfg.h \
+ ../uCOS-II/Source/ucos_ii.h ../uCOS-II/Source/os_cfg_r.h \
+ ../uCOS-II/Ports/AVR32/UC3/AtmelStudio6/os_cpu.h ../BSP/bsp.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/io.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/uc3a0512.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/core_sc0_140.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/abi.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/ocd_s0_140.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/intc_101.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/adc_110.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/abdac_101.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/eic_231.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/flashc_200.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/freqm_231.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/gpio_110.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/hmatrix_230.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/sdramc_220.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/smc_106.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/macb_112.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/pdca_102.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/pm_231.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/pwm_130.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/rtc_231.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/spi_1991.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/ssc_310.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/tc_222.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/twi_211.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/usart_400.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/usbb_311.h \
+ d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/wdt_231.h
+
+../APP/spi/spi.h:
+
+../APP/includes.h:
+
+../APP/os_cfg.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/stdio.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/newlib.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/config.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/ieeefp.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stddef.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stdarg.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/reent.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/_types.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_types.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_default_types.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/lock.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/types.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/types.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/features.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/stdio.h:
+
+../uC-CPU/AVR32/UC3/AtmelStudio6/cpu.h:
+
+../uC-CPU/cpu_def.h:
+
+../uC-CPU/Cfg/Template/cpu_cfg.h:
+
+../APP/app_cfg.h:
+
+../uCOS-II/Source/ucos_ii.h:
+
+../uCOS-II/Source/os_cfg_r.h:
+
+../uCOS-II/Ports/AVR32/UC3/AtmelStudio6/os_cpu.h:
+
+../BSP/bsp.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/io.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/uc3a0512.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/core_sc0_140.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/abi.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/ocd_s0_140.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/intc_101.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/adc_110.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/abdac_101.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/eic_231.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/flashc_200.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/freqm_231.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/gpio_110.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/hmatrix_230.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/sdramc_220.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/smc_106.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/macb_112.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/pdca_102.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/pm_231.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/pwm_130.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/rtc_231.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/spi_1991.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/ssc_310.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/tc_222.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/twi_211.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/usart_400.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/usbb_311.h:
+
+d:\software\atmel\ toolchain\avr32\ gcc\native\3.4.1057\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/avr32/wdt_231.h:
